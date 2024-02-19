4c2b8de3ec21cd4ca150261e615582cd9d8e594f
Atakan ERDOGAN <atakanerdogan001@gmail.com>
2024-02-19 14:11:53 +0300

    Add docstrings

--- kentcbs/kentcbs.py
+++ kentcbs/kentcbs.py
@@ -4,6 +4,14 @@ import random
 import string
 
 def generate_random_string(length=10):
+    """Generate a random string of a given length.
+
+    Args:
+        length (int, optional): Length of the random string. Defaults to 10.
+
+    Returns:
+        _type_: Random string of the given length.
+    """    
     # Define the pool of characters from which to choose
     characters = string.ascii_letters + string.digits + string.punctuation  # Include letters (both cases), digits, and punctuation
     
@@ -13,7 +21,14 @@ def generate_random_string(length=10):
     return random_string
 
 def lucky_number(length=3):
-    # Generate a random number
+    """_summary_
+
+    Args:
+        length (int, optional): _description_. Defaults to 3.
+
+    Returns:
+        _type_: _description_.
+    """    
     number = random.randint(0, 10**length - 1)
     
     return number
