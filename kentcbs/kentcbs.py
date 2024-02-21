0a7aade22862d6015ded6231f34b431610b1b2fa
Atakan ERDOGAN <atakanerdogan001@gmail.com>
2024-02-21 16:35:34 +0300

    Update after environment(kentcbs)

--- kentcbs/kentcbs.py
+++ kentcbs/kentcbs.py
@@ -13,12 +13,80 @@ class Map(ipyleaflet.Map):
 
         super().__init__(center=center, zoom=zoom,**kwargs)
 
+        if "layer_control" not in kwargs:
+           kwargs["layer_control"] = True
+        
+        if kwargs["search_control"]:    
+           self.add_layers_control()
+
 
 
     def add_search_control(self, position="topleft", **kwargs):
+        """_summary_
 
-        search_control = ipyleaflet.SearchControl(**kwargs)
+        Args:
+            position (str, optional): _description_. Defaults to "topleft".
+        """
+        if "url" not in kwargs:
+            kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"
+
+        search_control = ipyleaflet.SearchControl(position=position,**kwargs)
         self.add_control(search_control)
+
+    def add_draw_control(self, position="topleft", **kwargs):
+        """_summary_
+
+        Args:
+            position (str, optional): _description_. Defaults to "topleft".
+        """
+        draw_control = ipyleaflet.DrawControl(position=position,**kwargs)
+        self.add_control(draw_control)
+        """_summary_
+        """
+        draw_control.polyline = {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+
+        self.add_control(draw_control)
+
+        
+
+    
+    def add_layers_control(self, position='topright',**kwargs):
+
+        layers_control = ipyleaflet.LayersControl(position=position,**kwargs)
+        self.add_control(layers_control)
+
 
 
 
